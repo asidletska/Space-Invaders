3a46b9d4c45d9d0351bb10f6e88f44eba142407a
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-07-04 14:00:04 +0300

    Scripts Invaders

---
+++ Assets/Scripts/Invader.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    public Invaders[] prefabs; 
+    public int rows = 5;
+    public int columns = 11;
+    public float speed = 10.0f;
+    private Vector3 _direction = Vector2.right;
+    private void Awake()
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            float width = 2.0f * (columns - 1);
+            float height = 2.0f * (rows - 1);
+            Vector3 centering = new Vector3(-width / 2, -height / 2);
+            Vector3 rowPosition = new Vector3(centering.x, centering.y + (row * 2.0f), 0.0f );
+            for (int column = 0; column < columns; column++)
+            {
+                Invaders invader = Instantiate(prefabs[row], transform);
+                Vector3 position = rowPosition;
+                position.x += column * 2.0f;
+                invader.transform.localPosition = position;
+            }
+        }
+    }
+    private void Update()
+    {
+        transform.position += _direction * speed * Time.deltaTime;
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        foreach (Transform invader in this.transform)
+        {
+            if (invader.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+            if (_direction == Vector3.right && invader.position.x >= rightEdge.x - 1.0f)
+            {
+                Advancerow();
+            }
+            else if (_direction == Vector3.left && invader.position.x <= leftEdge.x + 1.0f)
+            {
+                Advancerow();
+            }
+        }
+    }
+    private void Advancerow()
+    {
+        _direction.x *= -1.0f;
+        Vector3 position = transform.position;
+        position.y -= 1.0f;
+        transform.position = position;
+    }
+}
--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Invaders : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite[] animationSprites;
+    public float animationTime = 1.0f;
 
-    // Update is called once per frame
-    void Update()
+    private SpriteRenderer _spriteRenderer;
+
+    private int _animationFrame;
+
+    private void Awake()
     {
-        
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    }
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
+
+    }
+    private void AnimateSprite()
+    {
+        _animationFrame++;
+
+        if (_animationFrame >= animationSprites.Length)
+        {
+            _animationFrame = 0;
+        }
+        _spriteRenderer.sprite = animationSprites[_animationFrame];
     }
 }
